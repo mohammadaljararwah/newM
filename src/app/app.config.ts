ba10c1b94c19e6e8c7efd8b6b090f2349f33115b
ahmad jaradat <ahmadggskull@gmail.com>
2025-10-17 14:49:27 +0300

    Feat: Major professional redesign of the homepage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router'; // Import withInMemoryScrolling
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(
+      routes,
+      // THE FIX IS HERE: Add this function to control scrolling
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+      })
+    ),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { VisionComponent } from './pages/vision/vision.component';
 import { GoalsComponent } from './pages/goals/goals.component';
-import { BrochureComponent } from './pages/brochure/brochure.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'vision', component: VisionComponent },
   { path: 'goals', component: GoalsComponent },
-  { path: 'brochure', component: BrochureComponent },
-  // The 'structure' path has been removed
-  { path: '**', redirectTo: '' } // Fallback for any other path
+  { path: '**', redirectTo: '' }
 ];
+
+@NgModule({
+  // THE FIX IS HERE: We added the options object below
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
